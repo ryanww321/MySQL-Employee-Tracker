975ddaf8843ca2fa630091078c775a8c9c9f7429
Ryan Whitcomb <ryanww321@gmail.com>
2020-02-25 20:54:01 -0600

    added inquirer prompts and some of the functions

--- seed.sql
+++ seed.sql
@@ -4,14 +4,52 @@ CREATE DATABASE company_db;
 
 USE company_db;
 
-CREATE TABLE products (
-  id INT NOT NULL AUTO_INCREMENT,
-  flavor VARCHAR(45) NULL,
-  price DECIMAL(10,2) NULL,
-  quantity INT NULL,
+
+CREATE TABLE employee (
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  department_id INT,
   PRIMARY KEY (id)
 );
 
-INSERT INTO products (flavor, price, quantity)
-VALUES ("vanilla", 2.50, 100);
+CREATE TABLE role (
+  id INT AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary INT,
+  department_id INT,
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE department (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(30),
+  PRIMARY KEY (id)
+);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id, department_id) 
+VALUES
+("Ryan", "Whitcomb", 1, null, 1),
+("Storey", "Sheldon", 2, 1, 2),
+("Francisco", "Cortez", 3, 1, 3),
+("Stefania", "Stefansdottir", 4, 1, 4),
+("Mason", "Scott", 5, 4, 5);
+
+INSERT INTO role (title, salary, department_id) 
+VALUES
+("CEO", 200000, 1),
+("CFO", 150000, 2),
+("CMO", 100000, 3),
+("CTO", 150000, 4),
+("IT", 50000, 5);
+
+INSERT INTO department (name) 
+VALUES
+("Head Office"),
+("Finance"),
+("Marketing"),
+("Technology"),
+("IT");
